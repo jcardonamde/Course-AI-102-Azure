527f58e6fbedfee7f225ee6a7feca3ef827d0044
Jonathan <yotomc17@gmail.com>
2023-07-24 21:51:28 -0500

    create chatbots and explore cognitive services

--- 12-qna/ask-question.cmd
+++ 12-qna/ask-question.cmd
@@ -2,7 +2,7 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set variables
-set prediction_url="YOUR_PREDICTION_ENDPOINT"
-set key="YOUR_KEY"
+set prediction_url="https://12qna-service.cognitiveservices.azure.com/language/:query-knowledgebases?projectName=LearnFAQ&api-version=2021-10-01&deploymentName=production"
+set key="9ea356d05c1f4301ae5508cd8cab1dc5"
 
 curl -X POST !prediction_url! -H "Ocp-Apim-Subscription-Key: !key!" -H "Content-Type: application/json" -d "{'question': 'What is a learning Path?' }"
